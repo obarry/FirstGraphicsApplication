f7a6576734bd8e6761b0b23bcf22f123f0bc339e
Olivier BARRY <olivier38b@gmail.com>
2020-11-24 00:41:24 +0100

    Implement graphics based on Microsoft Documentation's "Your First Direct2D Program" implemented statically.

--- FirstGraphicsApplication/FirstGraphicsApp.cpp
+++ FirstGraphicsApplication/FirstGraphicsApp.cpp
@@ -1,4 +1,8 @@
 #include <windows.h>
+#include <d2d1.h>
+#pragma comment(lib, "d2d1")
+
+
 #include <tchar.h>
 
 // Global variables
@@ -13,6 +17,16 @@ HINSTANCE hInst;
 
 // Forward declarations of functions included in this code module:
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void DiscardGraphicsResources();
+void OnPaint(HWND hWnd);
+void Resize(HWND hWnd);
+HRESULT CreateGraphicsResources(HWND hWnd);
+
+ID2D1Factory* pFactory;
+ID2D1HwndRenderTarget* pRenderTarget;
+ID2D1SolidColorBrush* pBrush;
+D2D1_ELLIPSE            ellipse;
+
 
 
 // WinMain function
@@ -129,25 +143,119 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		// To handle a WM_PAINT message, first call BeginPaint, then handle all the logic to lay out the text, buttons, and other controls in the window,
 		// and then call EndPaint. For the application, the logic between the beginning call and the ending call displays the string "Hello, Windows desktop!"
 		// in the window. In the following code, the TextOut function is used to display the string.
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-
-		// Here your application is laid out.
-		// For this introduction, we just print out "Hello, Windows desktop!"
-		// in the top left corner.
-		TextOut(hdc,
-			5, 5,
-			greeting, _tcslen(greeting));
-		// End application-specific layout section.
-
-		EndPaint(hWnd, &ps);
+	case WM_CREATE:
+		if (FAILED(D2D1CreateFactory(
+			D2D1_FACTORY_TYPE_SINGLE_THREADED, &pFactory)))
+		{
+			return -1;  // Fail CreateWindowEx.
+		}
 		break;
+
+	case WM_PAINT:
+		OnPaint(hWnd);
+		break;
+
 	case WM_DESTROY:
+		DiscardGraphicsResources();
+		 //SafeRelease(&pFactory);
 		PostQuitMessage(0);
 		break;
+
+	case WM_SIZE:
+		Resize(hWnd);
+		break;
+
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
 		break;
 	}
 	return 0;
 }
+
+
+void CalculateLayout()
+{
+	if (pRenderTarget != NULL)
+	{
+		D2D1_SIZE_F size = pRenderTarget->GetSize();
+		const float x = size.width / 2;
+		const float y = size.height / 2;
+		const float radius = min(x, y);
+		ellipse = D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius);
+	}
+
+}
+
+HRESULT CreateGraphicsResources(HWND hWnd)
+{
+	HRESULT hr = S_OK;
+	if (pRenderTarget == NULL)
+	{
+		RECT rc;
+		GetClientRect(hWnd, &rc);
+
+		D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+
+		hr = pFactory->CreateHwndRenderTarget(
+			D2D1::RenderTargetProperties(),
+			D2D1::HwndRenderTargetProperties(hWnd, size),
+			&pRenderTarget);
+
+		if (SUCCEEDED(hr))
+		{
+			const D2D1_COLOR_F color = D2D1::ColorF(1.0f, 1.0f, 0);
+			hr = pRenderTarget->CreateSolidColorBrush(color, &pBrush);
+
+			if (SUCCEEDED(hr))
+			{
+				CalculateLayout();
+			}
+		}
+	}
+	return hr;
+}
+
+void DiscardGraphicsResources()
+{
+	//SafeRelease(&pRenderTarget);
+	//SafeRelease(&pBrush);
+}
+
+void OnPaint(HWND hWnd)
+{
+	HRESULT hr = CreateGraphicsResources(hWnd);
+	if (SUCCEEDED(hr))
+	{
+		PAINTSTRUCT ps;
+		BeginPaint(hWnd, &ps);
+
+		pRenderTarget->BeginDraw();
+
+		pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::SkyBlue));
+		pRenderTarget->FillEllipse(ellipse, pBrush);
+
+		hr = pRenderTarget->EndDraw();
+		if (FAILED(hr) || hr == D2DERR_RECREATE_TARGET)
+		{
+			DiscardGraphicsResources();
+		}
+		EndPaint(hWnd, &ps);
+	}
+
+}
+
+void Resize(HWND hWnd)
+{
+	if (pRenderTarget != NULL)
+	{
+		RECT rc;
+		GetClientRect(hWnd, &rc);
+
+		D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+
+		pRenderTarget->Resize(size);
+		CalculateLayout();
+		InvalidateRect(hWnd, NULL, FALSE);
+	}
+}
+
